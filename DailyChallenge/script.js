f8aee1e5771fd82891812d9b523f987d790df7e1
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-06 20:08:03 +0000

    Add files via upload

---
+++ DailyChallenge/script.js
@@ -0,0 +1,24 @@
+let planets =[{name:"Mercury",moons:2},
+{name:"Venus",moons:1}, 
+{name:"Earth",moons:2},
+{name:"Mars", moons:3},
+{name:"Jupiter",moons:4},
+{name:"Saturn",moons:5},
+{name:"Uranus",moons:6},
+{name:"Nepturne",moons:7}];
+for (let planet of planets){
+    //console.log("planet :", planet)
+    let planetDiv=document.createElement("div")
+  
+    planetDiv.classList.add("planet", planet.name.toLowerCase());
+    for (let i=0; i<planet.moons; i++)
+    {
+        let moonDiv=document.createElement("div")
+        moonDiv.classList.add("moon")
+      
+        moonDiv.style.left=i*10+"px"
+        planetDiv.appendChild(moonDiv)
+    }
+    let section = document.querySelector(".listPlanets");
+    section?.appendChild(planetDiv);
+}
